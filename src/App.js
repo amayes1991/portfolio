f0ea858795f6dedfa4f74d5fd3bc1373dd7e580a
amayes1991 <84157250+amayes1991@users.noreply.github.com>
2022-04-22 01:50:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import Home from "./components/Home";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import About from "./components/About";
-import styled from "@emotion/styled";
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path="/about" element={<About />} />
+    <Home />
 
-    </Routes>
-    </BrowserRouter>
     </div>
   );
 }
